46f5c4b69598b2a1d25d610bb7bd0ebdc5c2ed21
Tara DeGuzman <tara.deguzman323@gmail.com>
2019-10-05 17:58:36 -0700

    first prompts added.

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
+var greeting = "Welcome!";
+var directions = "Select your criteria to generate your password."
+var alphaLtr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerLtr = "abcdefghijklmnopqrstuvwxyz";
+var characters = "!@#$%^&*()_-+=";
+var numbers = "0123456789";
 
-function generate (){
-    let complexity = document.getElementById("slider").value;
-
-    let value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=";
-
-    let password = "";
-
-    for(var i = 0; i <= complexity; i++){
-        password = password + value.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
-    }
-
-    document.getElementById("display").value = password;
-}
-
-document.getElementById("length").innerHTML = "Length: 25";
-
-document.getElementById("slider").oninput = function (){
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML="Length:" + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML="Length: 1";
-    }
-}
-
-function copyPassword (){
-    document.getElementById("display").select();
-    document.getElementById("copy");
-    alert ("Password copied to clipboard!");
-}
+var prompt = alert(greeting);
+var prompt = alert(directions);
